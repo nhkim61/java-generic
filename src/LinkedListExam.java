b69b4dd233f1f00f1bfe669b03cd064450056283
nhkim <nhkim61@gmail.com>
2020-11-04 01:22:34 +0900

    add generic method

--- src/LinkedListExam.java
+++ src/LinkedListExam.java
@@ -7,18 +7,45 @@ class LinkedListExam {
     public static void main(String[] args) {
 	System.out.println("LinkedListExam");
 	List<Integer> myIntList = new LinkedList<>();
-	myIntList.add(new Integer(0));
+	myIntList.add(Integer.valueOf(0));
 	Integer x = myIntList.iterator().next();
 	System.out.println(x);
 
 	List<String> ls = new ArrayList<String>();
 
-	Collection<?> c = new ArrayList<String>();
-	//c.add("111");
+	Collection<String> c = new ArrayList<String>();
+	c.add("111");
+	printCollection(c);
+
+	Object[] oa = new Object[10];
+	Collection<Object> co = new ArrayList<Object>();
+	//fromArrayToCollection(oa,co);
+	//printCollection(co);
+
+	String[] sa = new String[10];
+	Collection<String> cs = new ArrayList<String>();
+	//fromArrayToCollection(sa,cs);
+	//fromArrayToCollection(sa,co);
+	
+	//printCollection(co);
+
+	Integer[] ia = new Integer[10];
+	Float[] fa = new Float[10];
+	Number[] na = new Number[10];
+
+	Collection<Number> cn = new ArrayList<Number>();
+	fromArrayToCollection(na, co);
+	printCollection(co);
+	
     }
-    void printCollection(Collection<?> c) {
+    static void printCollection(Collection<?> c) {
 	for(Object e : c) {
 	    System.out.println(e);
+	}
+    }
+    static <T> void fromArrayToCollection(T[] a, Collection<T> c) {
+	for(T o:a){
+	    c.add(o);
 	}
     }
 }
